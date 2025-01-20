a3a7a1226549ac15e613ba8632980e7e0d358a00
Laurent Tempier <laurent.tempier@gmail.com>
2025-01-20 22:19:23 +0100

    rm background

--- scripts.js
+++ scripts.js
@@ -1,32 +1,33 @@
+
 window.onload = function () {
 
-   const colors = [
-      '#E7B0CD',
-      '#FFECCC',
-      '#D1C6E5',
-      '#B5D0DC',
-      '#90235E',
-      '#AAD432'
-   ];
+   // const colors = [
+   //    '#E7B0CD',
+   //    '#FFECCC',
+   //    '#D1C6E5',
+   //    '#B5D0DC',
+   //    '#90235E',
+   //    '#AAD432'
+   // ];
 
-   const numCircles = 10;  // Nombre de cercles à générer (10 cercles ici)
-   const container = document.querySelector('.full-page-background'); // Conteneur pour les cercles
+   // const numCircles = 10;  // Nombre de cercles à générer (10 cercles ici)
+   // const container = document.querySelector('.full-page-background'); // Conteneur pour les cercles
 
-   for (let i = 0; i < numCircles; i++) {
-      const circle = document.createElement('div');
-      circle.classList.add('circle');
+   // for (let i = 0; i < numCircles; i++) {
+   //    const circle = document.createElement('div');
+   //    circle.classList.add('circle');
 
-      const size = Math.floor(Math.random() * (300 - 100 + 1)) + 100; // Taille entre 100px et 300px
-      circle.style.width = `${size}px`;
-      circle.style.height = `${size}px`;
+   //    const size = Math.floor(Math.random() * (300 - 100 + 1)) + 100; // Taille entre 100px et 300px
+   //    circle.style.width = `${size}px`;
+   //    circle.style.height = `${size}px`;
 
-      // Positionner aléatoirement le cercle
-      circle.style.top = `${Math.floor(Math.random() * 100)}%`;   // Position verticale entre 0 et 100%
-      circle.style.left = `${Math.floor(Math.random() * 100)}%`;  // Position horizontale entre 0 et 100%
+   //    // Positionner aléatoirement le cercle
+   //    circle.style.top = `${Math.floor(Math.random() * 100)}%`;   // Position verticale entre 0 et 100%
+   //    circle.style.left = `${Math.floor(Math.random() * 100)}%`;  // Position horizontale entre 0 et 100%
 
-      circle.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+   //    circle.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 
-      container.appendChild(circle);
-   }
+   //    container.appendChild(circle);
+   // }
 
 };
